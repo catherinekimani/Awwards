a28c4dcfe717ed59344a941fe19c55b866d8d479
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-12 00:35:15 +0300

    Ft:Create profile model

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_profile = CloudinaryField('pictures')
+    bio = models.TextField()
+    contact = models.EmailField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
