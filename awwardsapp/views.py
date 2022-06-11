83ff8a7a08d87ac9b9f7f97558072c01bb384a0d
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-12 00:13:28 +0300

    Ft: Ft: Create login_user function

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 
 from django.contrib.auth.forms import UserCreationForm
-from .forms import RegisterForm
+from .forms import RegisterForm,LoginForm
+from django.contrib.auth import login,logout,authenticate
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -13,5 +14,19 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect('login')
     return render(request,'register.html',{'form':form})
+
+def login_user(request):
+    form = LoginForm()
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+        
+        user = authenticate(request, username = username, password = password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+    return render(request, 'login.html',{'form':form})
