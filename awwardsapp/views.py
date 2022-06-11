0e856ed6bd1bb7147b2c4c422fc3b6e3fda3264c
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-11 22:26:39 +0300

    Ft:Create register view function

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def register(request):
+    form = RegisterForm()
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request,'register.html',{'form':form})
