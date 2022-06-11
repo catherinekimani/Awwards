7a0c71b3ba0951f163d2a66e7a516aa4a6ca540c
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-12 00:12:32 +0300

    Ft:Create login form

--- awwardsapp/forms.py
+++ awwardsapp/forms.py
@@ -15,3 +15,15 @@ class RegisterForm(UserCreationForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+        
+class LoginForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','password1']
+    
+    def __init__(self,*args, **kwargs):
+        super(LoginForm, self).__init__(*args,**kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        
