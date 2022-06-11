116a36bff439314a050e4d2a834407b33290e952
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-12 02:40:29 +0300

    Ft: Add url for profile view function

--- awwardsapp/urls.py
+++ awwardsapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     
     path('register/',views.register,name='register'),
-    path('login/', views.login_user,name='login')
+    path('login/', views.login_user,name='login'),
+    
+    path('profile/',views.profile,name='profile')
 ]
