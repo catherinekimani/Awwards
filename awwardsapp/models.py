2b9d960705660171eb3aa02b24019136c53bbb4c
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-12 20:08:29 +0300

    Ft:Create post model class

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -12,3 +12,15 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+class Post(models.Model):
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=400)
+    url = models.URLField(max_length=200)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    technologies = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f'{self.description}'
