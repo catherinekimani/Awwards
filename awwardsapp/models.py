3146c9968073aab1da121ec514a1722d750f9aaf
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-14 22:18:28 +0300

    Ft:Create rating modelclass

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 from cloudinary.models import CloudinaryField
 
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -22,5 +23,33 @@ class Post(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     technologies = models.CharField(max_length=200)
     
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+    
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+    
     def __str__(self):
         return f'{self.description}'
+    
+    
+class Rating(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    design_rate = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)],default=0)
+    usability_rate = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)],default=0)
+    content_rate = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)],default=0) 
+    date_posted = models.DateTimeField(auto_now_add=True)
+    
+    def save_rating(self):
+        self.save()
+
+    def delete_rating(self):
+        self.delete()
+    
+    def __str__(self):
+        return f'{self.post}'
