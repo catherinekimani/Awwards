cdeac3b6970119847cdf8fa959dc8fa37e6775bb
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-15 10:37:36 +0300

    Run post & Rating tests

--- awwardsapp/tests.py
+++ awwardsapp/tests.py
@@ -14,3 +14,31 @@ class ProfileTest(TestCase):
 
     def test_delete_user(self):
         self.user.delete()
+        
+class PostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='moringa',email="moringa@gmail.com")
+        self.post = Post.objects.create(title='Personal Blog', image='img.png', description='project test', user=self.user, url="https://github.com/catherinekimani/personal-blog")
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+        
+    def test_save_post(self):
+        self.post.save()
+        
+    def test_delete_post(self):
+        self.post.save()
+        
+class RatingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='moringa',email="moringa@gmail.com")
+        self.post = Post.objects.create(title='Personal Blog', image='img.png', description='project test', user=self.user, url="https://github.com/catherinekimani/personal-blog")
+        self.rating = Rating.objects.create(design_rate=4,usability_rate=7,content_rate=2,user=self.user,post=self.post,date_posted=2022-6-15)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
+        
+    def test_save_rate(self):
+        self.post.save()
+        
+    def test_delete_rate(self):
+        self.post.save()
+        
