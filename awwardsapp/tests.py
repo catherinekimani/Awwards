dea57ed0ea5beefedfacf41d47062fdf1e4f586a
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-15 03:35:32 +0300

    Ft:Run profile test

--- awwardsapp/tests.py
+++ awwardsapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.user = User(username='moringa',email="moringa@gmail.com", password='kate')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
