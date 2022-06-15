16ea9829a32f7f99adca655cb2b17f31dfbfccc9
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-15 03:18:34 +0300

    Ft:Add logout,search & rate urls

--- awwardsapp/urls.py
+++ awwardsapp/urls.py
@@ -6,11 +6,15 @@ urlpatterns = [
     
     path('register/',views.register,name='register'),
     path('login/', views.login_user,name='login'),
+    path('logout/',views.logout_user,name='logout'),
     
     path('profile/',views.profile,name='profile'),
     path('edit-profile',views.edit_profile,name='edit-profile'),
     
     path('project',views.project,name='project'),
     path('api/post/',views.ProjectList.as_view(),name=''),
-    path('api/profile/',views.ProfileList.as_view(),name='')
+    path('api/profile/',views.ProfileList.as_view(),name=''),
+    
+    path('search/', views.search_results, name='search_results'),
+    path("rate/<post_id>/",views.rate, name='rate'),
 ]
