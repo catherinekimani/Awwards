20441becdffe71795e3fe3e9af195158afb5838b
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-16 13:53:47 +0300

    Ft:Add slight changes

--- awwardsapp/apps.py
+++ awwardsapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AwwardsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'awwardsapp'
+    
+    def ready(self):
+        import awwardsapp.signals
