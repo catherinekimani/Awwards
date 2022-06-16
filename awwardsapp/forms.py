eb7e70d518ebfff52c7bbd4f4bbe878b83bdd089
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-16 13:55:53 +0300

    Change description to charfield

--- awwardsapp/forms.py
+++ awwardsapp/forms.py
@@ -35,16 +35,17 @@ class LoginForm(UserCreationForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['bio','user','user_profile']
+        fields = ['bio','user']
         
 class ProfileUpdateForm(forms.ModelForm):
     bio = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control'}))
     
     class Meta:
         model = Profile
-        fields = ['bio','user_profile','contact']
+        fields = ['bio','contact','user_profile']
         
 class PostProjectForm(forms.ModelForm):
+    description = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model = Post
         fields = ['image','title','description','url','technologies']
